56a0e1ff7157bb7917938c5ded3ec21b1ab6fcd3
Ivan Knysh <mr.rauber.ik@gmail.com>
2023-11-11 10:07:19 +0200

    Added task 2

--- scripts.js
+++ scripts.js
@@ -3,3 +3,12 @@
  */
 const pow = (num, degree) => (degree === 0 ? 1 : num * pow(num, degree - 1));
 console.log(pow(5, 3));
+
+/**
+ * Завдання 2. Реалізуйте функцію removeElement
+ */
+const removeElement = (arr, item) => arr.splice(arr.indexOf(item), 1);
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5);
+console.log(array);
