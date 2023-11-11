aba48461921b5d9059d60d4ec0b17fd95d1d25d0
Ivan Knysh <mr.rauber.ik@gmail.com>
2023-11-11 10:10:16 +0200

    Added task 3

--- scripts.js
+++ scripts.js
@@ -12,3 +12,20 @@ const removeElement = (arr, item) => arr.splice(arr.indexOf(item), 1);
 const array = [1, 2, 3, 4, 5, 6, 7];
 removeElement(array, 5);
 console.log(array);
+
+/**
+ * Завдання 3. Реалізуйте функцію generateKey
+ */
+const generateKey = (length, characters) => {
+	let result = "";
+
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * characters.length);
+		result += characters.charAt(randomIndex);
+	}
+
+	return result;
+};
+
+const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+console.log(generateKey(16, characters));
